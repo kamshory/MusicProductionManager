ac022772207006d41296d4035df94e3e3b442e25
Kamshory, MT <kamshory@yahoo.com>
2024-02-23 00:46:55 +0700

    Update title

--- login.php
+++ login.php
@@ -59,7 +59,7 @@ if($inputPost->getUsername() != null && $inputPost->getPassword() != null)
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title><?php echo $cfg->getAppName();?></title>
   <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="assets/css/styles.min.css" />
 </head>
