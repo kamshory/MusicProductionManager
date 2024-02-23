00e27e2e1ea2f6439e7e87f2be8094cf1ba9a36e
Kamshory, MT <kamshory@yahoo.com>
2024-02-24 02:06:45 +0700

    Update

--- register.php
+++ register.php
@@ -20,7 +20,7 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                <a href="./" class="text-nowrap logo-img text-center d-block py-3 w-100">
                   <img src="assets/images/logos/dark-logo.png" width="220" alt="">
                 </a>
                 <form>
