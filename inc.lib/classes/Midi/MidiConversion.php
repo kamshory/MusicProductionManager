f5378f1a04a7c9f742d05c569d3937f7e004dbd4
Kamshory, MT <kamshory@yahoo.com>
2024-03-04 15:10:59 +0700

    Update dialog

--- inc.lib/classes/Midi/MidiConversion.php
+++ inc.lib/classes/Midi/MidiConversion.php
@@ -17,7 +17,36 @@ class MidiConversion extends Midi
 			array_pop($track); // remove Meta TrkEnd
 			$singleTrack = array_merge($singleTrack, $track);
 		}
-		usort($singleTrack, create_function('$a,$b', '$ta =(int)strtok($a,\' \');$tb=(int)strtok($b,\' \');return $ta==$tb?0:($ta<$tb?-1:1);'));
+		/*
+		Old code
+		=> usort
+		=> ($singleTrack, create_function('$a,$b', '$ta =(int)strtok($a,\' \');$tb=(int)strtok($b,\' \');return $ta==$tb?0:($ta<$tb?-1:1);'));
+		*/
+		
+		if (version_compare(phpversion(), '7.2', '>='))
+		{
+			usort($singleTrack, function($a, $b){
+				$ta = (int) strtok ($a, ' ');
+				$tb = (int) strtok ($b, ' ');
+				if($ta == $tb)
+				{
+					return 0;
+				}
+				else if($ta < $tb)
+				{
+					return -1;
+				}
+				else
+				{
+					return 1;
+				}
+			});
+		}
+		else
+		{
+			usort($singleTrack, create_function('$a,$b', '$ta =(int)strtok($a,\' \');$tb=(int)strtok($b,\' \');return $ta==$tb?0:($ta<$tb?-1:1);'));
+		}
+		
 		$endTime = strtok($singleTrack[count($singleTrack) - 1], " ");
 		$singleTrack[] = "$endTime Meta TrkEnd";
 		$this->tracks = array($singleTrack);
