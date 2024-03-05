5546f058e53e7f541eb93da9a636b38ca74f1db4
Kamshory, MT <kamshory@yahoo.com>
2024-03-05 15:32:03 +0700

    Update

---
+++ inc.lib/classes/MusicProductionManager/Utility/SongUtil.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace MusicProductionManager\Utility;
+
+use Exception;
+use MagicObject\Database\PicoDatabase;
+use MagicObject\Exceptions\NoRecordFoundException;
+use MusicProductionManager\Data\Entity\Rating;
+
+class SongUtil
+{
+    /**
+     * Set rating
+     *
+     * @param PicoDatabase $database
+     * @param string $songId
+     * @param string $userId
+     * @param string $time
+     * @return void
+     */
+    public static function setRating($database, $songId, $userId, $rating, $time)
+    {
+        $songRating = new Rating(null, $database);
+        try
+        {
+            $songRating->findOneBySongIdAndUserId($songId, $userId);
+            $songRating->setRating($rating);
+            $songRating->setTimeEdit($time);
+            $songRating->update();
+        }
+        catch(NoRecordFoundException $e)
+        {
+            $songRating->setSongId($songId);
+            $songRating->setUserId($userId);
+            $songRating->setRating($rating);
+            $songRating->setTimeCreate($time);
+            $songRating->setTimeEdit($time);
+            $songRating->insert();
+        }
+        catch(Exception $e)
+        {
+            // do nothing
+        }
+    }
+    /**
+     * Get song rating
+     *
+     * @param PicoDatabase $database
+     * @param string $songId
+     * @return float
+     */
+    public static function getRating($database, $songId)
+    {
+        $ratings = new Rating(null, $database);
+        $ratings->findBySongId($songId);
+        $result = $ratings->getResult();
+        $sum = 0;
+        foreach($result as $row)
+        {
+            $sum += $row->getRating();
+        }
+        if(empty($result))
+        {
+            $allRating = 0.0;
+        }
+        else
+        {
+            $allRating = $sum / count($result);
+        }
+        return $allRating;
+    }
+}
