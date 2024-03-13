ad7bec63ec9f62cc82f4a8d5ac6566540435c2e2
Kamshory, MT <kamshory@yahoo.com>
2024-03-13 09:36:29 +0700

    Create install.php

---
+++ install.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Install</title>
+</head>
+<body>
+    
+<div class="all">
+    <div class="form">
+        <form action="">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Application Name</td>
+                        <td><input type="text" name="application_name" id="application_name"></td>
+                    </tr>
+                    <tr>
+                        <td>Base URL</td>
+                        <td><input type="url" name="base_url" id="base_url"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Host</td>
+                        <td><input type="text" name="database_host" id="database_host"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Port</td>
+                        <td><input type="number" step="1" min="0" max="65535" name="database_port" id="database_port"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Schema</td>
+                        <td><input type="text" name="database_schema" id="database_schema"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Name</td>
+                        <td><input type="text" name="database_name" id="database_name"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Username</td>
+                        <td><input type="text" name="database_usernname" id="database_usernname"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Password</td>
+                        <td><input type="text" name="database_password" id="database_password"></td>
+                    </tr>
+                    <tr>
+                        <td>Database Salt</td>
+                        <td><input type="text" name="database_salt" id="database_salt"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+</div>
+    
+</body>
+</html>
