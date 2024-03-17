1f10959f85909f1c54931604fb4cc08699ad152a
Kamshory, MT <kamshory@yahoo.com>
2024-03-18 03:21:38 +0700

    Add article

---
+++ inc.lib/classes/MusicProductionManager/Data/Entity/Article.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace MusicProductionManager\Data\Entity;
+
+use MagicObject\MagicObject;
+
+/**
+ * @Entity
+ * @JSON(property-naming-strategy=SNAKE_CASE)
+ * @Table(name="article")
+ */
+class Article extends MagicObject
+{
+	/**
+	 * Article ID
+	 * 
+	 * @NotNull
+	 * @Column(name="article_id", type="varchar(40)", length=40, nullable=false)
+	 * @var string
+	 */
+	protected $articleId;
+
+	/**
+	 * Title
+	 * 
+	 * @Column(name="title", type="text", nullable=true)
+	 * @var string
+	 */
+	protected $title;
+
+	/**
+	 * Content
+	 * 
+	 * @Column(name="content", type="longtext", nullable=true)
+	 * @var string
+	 */
+	protected $content;
+
+	/**
+	 * Time Create
+	 * 
+	 * @Column(name="time_create", type="timestamp", length=19, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $timeCreate;
+
+	/**
+	 * Time Edit
+	 * 
+	 * @Column(name="time_edit", type="timestamp", length=19, nullable=true)
+	 * @var string
+	 */
+	protected $timeEdit;
+
+	/**
+	 * Admin Create
+	 * 
+	 * @Column(name="admin_create", type="varchar(40)", length=40, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $adminCreate;
+
+	/**
+	 * Admin Edit
+	 * 
+	 * @Column(name="admin_edit", type="varchar(40)", length=40, nullable=true)
+	 * @var string
+	 */
+	protected $adminEdit;
+
+	/**
+	 * IP Create
+	 * 
+	 * @Column(name="ip_create", type="varchar(50)", length=50, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $ipCreate;
+
+	/**
+	 * IP Edit
+	 * 
+	 * @Column(name="ip_edit", type="varchar(50)", length=50, nullable=true)
+	 * @var string
+	 */
+	protected $ipEdit;
+
+	/**
+	 * Draft
+	 * 
+	 * @Column(name="draft", type="tinyint(1)", length=1, default_value="1", nullable=true)
+	 * @DefaultColumn(value="1")
+	 * @var bool
+	 */
+	protected $draft;
+
+	/**
+	 * Active
+	 * 
+	 * @Column(name="active", type="tinyint(1)", length=1, default_value="1", nullable=true)
+	 * @DefaultColumn(value="1")
+	 * @var bool
+	 */
+	protected $active;
+
+}
