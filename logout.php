69ecb6199b907826f66426d495e8550994cc72b6
Kamshory, MT <kamshory@yahoo.com>
2024-03-25 13:16:54 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,8 +2,7 @@
 
 use MusicProductionManager\Utility\UserUtil;
 
-require_once "inc/app.php";
-require_once "inc/session.php";
+require_once "inc/auth.php";
 
 UserUtil::logUserActivity($cfg, $database, $currentLoggedInUser->getUserId(), "Logout from system", null, null, true);
 
