b77e1035f648286e25fc505d38aefd33cd0320fc
Kamshory, MT <kamshory@yahoo.com>
2024-03-25 12:58:02 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,10 @@
 <?php
 
-use MagicObject\Request\InputPost;
-use MusicProductionManager\Data\Entity\EntityUser;
-use MusicProductionManager\Utility\ServerUtil;
 use MusicProductionManager\Utility\UserUtil;
 
 require_once "inc/app.php";
 require_once "inc/session.php";
+
+UserUtil::logUserActivity($cfg, $database, $currentLoggedInUser->getUserId(), "Logout from system", null, null, true);
 
 $sessions->destroy();
