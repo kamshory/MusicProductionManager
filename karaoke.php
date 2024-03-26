910a8ca1c474f53d1ed47161b8f414a0a0c6fa35
Kamshory, MT <kamshory@yahoo.com>
2024-03-26 18:29:57 +0700

    Update karaoke.php

--- karaoke.php
+++ karaoke.php
@@ -42,7 +42,7 @@ require_once "inc/header.php";
 <link rel="stylesheet" href="assets/css/piano.css">
 <script src="assets/js/karaoke.js"></script>
 <script src="assets/js/piano.js"></script>
-<script src="assets/soundfont/soundfont-player.min.js"></script>
+<script src="assets/soundfont/soundfont-player.js"></script>
 <script src="assets/soundfont/soundfont-midi-player.js?rand=<?php echo mt_rand(1, 999999);?>"></script>
 
   <form action="" method="get">
