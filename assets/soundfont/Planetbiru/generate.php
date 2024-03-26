47d91b90316d50b636d6a0b9e963ac13927ee8b1
Kamshory, MT <kamshory@yahoo.com>
2024-03-27 04:45:09 +0700

    Add soundfont saxophone

--- assets/soundfont/Planetbiru/generate.php
+++ assets/soundfont/Planetbiru/generate.php
@@ -1,36 +1,64 @@
 <?php
 
-$directory = "";
+$name = "";
 
 if($argc > 1)
 {
-    $directory = trim($argv[1]);
+    $name = trim($argv[1]);
 }
 else
 {
-    if(isset($_GET['directory']))
+    if(isset($_GET['name']))
     {
-        $directory = trim($_GET['directory']);
+        $name = trim($_GET['name']);
     }
 }
-$name = $directory;
-$directory .= "-mp3";
-$path = __DIR__."/$directory.js";
-$list = glob(__DIR__."/$directory/*.mp3");
-$fp = fopen($path, "w");
 
-fwrite($fp, "if (typeof(MIDI) === 'undefined') var MIDI = {};
-if (typeof(MIDI.Soundfont) === 'undefined') MIDI.Soundfont = {};
-MIDI.Soundfont.$name = {\r\n");
+$listMp3 = glob(__DIR__."/$name/*.mp3");
+$listOgg = glob(__DIR__."/$name/*.ogg");
 
-foreach($list as $file)
+if(count($listMp3) > 80)
 {
-    $baseName = basename($file);
-    if(stripos($baseName, ".") !== false)
+    echo "Generate Soundfont mp3\r\n";
+    $directoryMp3 = $name;
+    $directoryMp3 .= "-mp3";
+    $pathMp3 = __DIR__."/$directoryMp3.js";
+    $fpMp3 = fopen($pathMp3, "w");
+
+    fwrite($fpMp3, "if (typeof(MIDI) === 'undefined') var MIDI = {};\r\nif (typeof(MIDI.Soundfont) === 'undefined') MIDI.Soundfont = {};\r\nMIDI.Soundfont.$name = {\r\n");
+
+    foreach($listMp3 as $file)
     {
-        $arr = explode(".", $baseName);
-        $baseName = $arr[0];
+        $baseName = basename($file);
+        if(stripos($baseName, ".") !== false)
+        {
+            $arr = explode(".", $baseName);
+            $baseName = $arr[0];
+        }
+        fwrite($fpMp3, "\"$baseName\":\"data:audio/mp3;base64,".base64_encode(file_get_contents($file))."\",\r\n");
     }
-    fwrite($fp, "\"$baseName\":\"data:audio/mp3;base64,".base64_encode(file_get_contents($file))."\",\r\n");
+    fwrite($fpMp3, "}\r\n");
 }
-fwrite($fp, "}\r\n");
+
+if(count($listOgg) > 80)
+{
+    echo "Generate Soundfont ogg\r\n";
+    $directoryOgg = $name;
+    $directoryOgg .= "-ogg";
+    $pathOgg = __DIR__."/$directoryOgg.js";
+    $fpOgg = fopen($pathOgg, "w");
+
+    fwrite($fpOgg, "if (typeof(MIDI) === 'undefined') var MIDI = {};\r\nif (typeof(MIDI.Soundfont) === 'undefined') MIDI.Soundfont = {};\r\nMIDI.Soundfont.$name = {\r\n");
+
+    foreach($listOgg as $file)
+    {
+        $baseName = basename($file);
+        if(stripos($baseName, ".") !== false)
+        {
+            $arr = explode(".", $baseName);
+            $baseName = $arr[0];
+        }
+        fwrite($fpOgg, "\"$baseName\":\"data:audio/ogg;base64,".base64_encode(file_get_contents($file))."\",\r\n");
+    }
+    fwrite($fpOgg, "}\r\n");
+}
