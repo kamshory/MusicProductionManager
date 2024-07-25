000eb693fc6c7811ea815b466778ea6d666701d0
Kamshory, MT <kamshory@yahoo.com>
2024-07-25 23:21:24 +0700

    Update

--- inc/session.php
+++ inc/session.php
@@ -7,5 +7,5 @@ require_once dirname(__DIR__)."/inc.lib/vendor/autoload.php";
 
 $sessConf = new SecretObject($cfg->getSession());
 $sessions = new PicoSession($sessConf);
-
+$sessions->setSessionCookieParams($sessConf->getMaxLifeTime(), $sessConf->isCookieSecure(), $sessConf->isCookieHttpOnly());
 $sessions->startSession();
